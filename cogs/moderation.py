6628d7b59857b497cc825ab02bb81b2f154edaa2
superhylia <43157926+superhylia@users.noreply.github.com>
2020-08-31 12:38:29 -0500

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -329,20 +329,7 @@ class Moderation(commands.Cog):
         await ctx.send(f'Deleted {limit - count} messages', delete_after=3)
         await self.send_log(ctx, limit - count, member)
  
-    @group(6, invoke_without_command=True, usage='')
-    async def lockdown(self, ctx, channel: discord.TextChannel=None):
-        """Lockdown channels or the server."""
-        await ctx.invoke(self.bot.get_command('help'), command_or_cog='lockdown')
-        try:
-            channel = await channel or ctx.channel
-        except commands.BadArgument:
-            await ctx.invoke(self.bot.get_command('help'), command_or_cog='lockdown')
-        else:
-            ctx.command = self.add_
-            await ctx.invoke(self.add_, channel: discord.TextChannel=TextChannel)
-
-
-    @lockdown.command(6, aliases=['lock', 'closedown', 'unlock', 'unlockdown'])
+    @command(6, aliases=['lock', 'closedown', 'unlock', 'unlockdown'])
     async def lockdown(self, ctx, channel: discord.TextChannel=None):
         """Lock/unlock a specified channel."""
         channel = channel or ctx.channel
@@ -362,23 +349,6 @@ class Moderation(commands.Cog):
 
         await self.send_log(ctx, enable, channel)
      
-    @lockdown.command(6, aliases=['serverwide', 'everything'])
-    async def server(self, ctx, channel: discord.TextChannel=None):
-        """Lock/unlock the server."""
-    
-        for channel in guild.channels:
-            if overwrite.send_messages is None or overwrite.send_messages:
-                overwrite.send_messages = False
-                await channel.set_permissions(cts.guild.default_role, overwrite=overwrite)
-                await ctx.send(f'The server has been locked. {self.bot.accept}')
-                enable = True
-            else:
-                # dont change to "not overwrite.send_messages"
-                overwrite.send_messages = None
-                await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
-                await ctx.send(f'The server has been unlocked. {self.bot.accept}')
-                enable = False
-
     @command(6, usage='[duration] [channel]')
     async def slowmode(self, ctx, *, time: UserFriendlyTime(converter=commands.TextChannelConverter, default=False, assume_reason=True)):
         """Enables slowmode in the specified channel. Max. 6h period.
